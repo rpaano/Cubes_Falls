60d99f7c491b82cf3abd7b3809e35f5b8e305704
rpaano <50399801+rpaano0804@users.noreply.github.com>
2019-05-10 18:16:52 +0800

    Level 2
    Animation, Script Scroll and Repeat Background.

--- Cube_Falls/Assets/Scripts/BG Scripts/BGScroll.cs
+++ Cube_Falls/Assets/Scripts/BG Scripts/BGScroll.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class BGScroll : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float scroll_Speed = 0.3f;
+
+    private MeshRenderer mesh_Renderer;
+
+
+	void Awake () {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Scroll();
 	}
-}
+
+    void Scroll(){
+
+        Vector2 offset = mesh_Renderer.sharedMaterial.GetTextureOffset("_MainTex");
+        offset.y += Time.deltaTime * scroll_Speed;
+
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+
+
+    }
+
+
+} //class
+
+
