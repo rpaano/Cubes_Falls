bccc207354799d0bb717e2fe949817ac8c35a516
rpaano <50399801+rpaano0804@users.noreply.github.com>
2019-05-11 16:37:57 +0800

    Version 2
    Has save the last one..

---
+++ Cube_Falls/Assets/Scripts/Platform Scripts/PlatformScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+
+    public float move_Speed = 2f;
+    public float bound_Y = 6f;
+
+    public bool moving_Platform_Left, moving_Platform_Right, is_Breakable, is_Spike, is_Platform;
+
+    private Animator anim;
+
+    void Awake(){
+        if (is_Breakable)
+            anim = GetComponent<Animator>();
+            
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+	}
+
+    void move(){
+
+        Vector2 temp = transform.position;
+        temp.y += move_Speed * Time.deltaTime;
+        transform.position = temp;
+
+        if(temp.y >= bound_Y){
+            gameObject.SetActive(false);
+        }
+    }
+
+}
